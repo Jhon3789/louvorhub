00c7ed7da27aa6273defa716b087d60de7a01dba
Jhonny Garcia <garciajhonnyjose49@gmail.com>
2026-07-09 18:29:29 -0300

    teste authguard

--- app/components/AuthGuard.tsx
+++ app/components/AuthGuard.tsx
@@ -1,38 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { supabase } from "../../lib/supabase";
-import { useRouter } from "next/navigation";
-
 export default function AuthGuard({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-  const [carregando, setCarregando] = useState(true);
-
-  useEffect(() => {
-    async function verificar() {
-      const { data } = await supabase.auth.getSession();
-
-      if (!data.session) {
-        router.replace("/login");
-      } else {
-        setCarregando(false);
-      }
-    }
-
-    verificar();
-  }, [router]);
-
-  if (carregando) {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-zinc-950 text-white">
-        Carregando...
-      </div>
-    );
-  }
-
   return <>{children}</>;
 }
