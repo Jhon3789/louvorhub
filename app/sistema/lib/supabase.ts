76e30047f5f6698ce6fc5c44319deb45731fa9b5
Jhonny Garcia <garciajhonnyjose49@gmail.com>
2026-07-10 15:18:33 -0300

    corrige supabase admin key

--- app/sistema/lib/supabase.ts
+++ app/sistema/lib/supabase.ts
@@ -3,10 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 
-export const supabase = createClient(
+export const supabaseAdmin = createClient(
   supabaseUrl,
-  supabaseKey
+  serviceRoleKey
 );
