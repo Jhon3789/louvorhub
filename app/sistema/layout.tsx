75070895ffa5e72eb87d55ccea5da55efd4a2a24
Jhonny Garcia <garciajhonnyjose49@gmail.com>
2026-07-11 17:55:26 -0300

    corrige sidebar e layout sistema

--- app/sistema/layout.tsx
+++ app/sistema/layout.tsx
@@ -13,14 +13,14 @@ export default function SistemaLayout({
     <div className="flex min-h-screen bg-black">
 
 
-      <Sidebar />
+      <div>
+        <Sidebar />
+      </div>
 
 
       <main className="flex-1 p-6">
 
-
         {children}
-
 
       </main>
 
